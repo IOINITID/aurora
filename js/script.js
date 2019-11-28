b3350eab166eacb2beb316c8a75afdd1e146c10a
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-11-28 18:02:21 +0300

    Added all pages, adaptation for mobile devices and modals

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// slider-two element
+// slider element
 $('.slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  // autoplay: true,
+  autoplay: true,
   autoplaySpeed: 5000,
   centerMode: true,
   centerPadding: '290px',
@@ -11,9 +11,37 @@ $('.slider').slick({
   responsive: [{
     breakpoint: 1300,
     settings: {
-      arrows: false,
       slidesToShow: 1,
-      dots: true
+      slidesToScroll: 1,
+      autoplay: false,
+      centerMode: false,
+      arrows: false,
+      dots: true,
     }
-  }]
+  }],
+});
+// popup
+$('.open-popup-link').magnificPopup({
+  type: 'inline',
+  midClick: true,
+});
+// popup
+$('.popup-close').on('click', () => {
+  $.magnificPopup.close();
+});
+// header fixed
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 400) {
+    $('.header--fixed').removeClass('hide');
+  } else {
+    $('.header--fixed').addClass('hide');
+  }
+});
+// input mobile text hide
+$('.popup input').on('focus', () => {
+  $('.popup input').attr("placeholder", "").placeholder();
+});
+// input mobile text show
+$('.popup input').on('focusout', () => {
+  $('.popup input').attr("placeholder", "Номер телефона").placeholder();
 });
--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@
 
 Главная страница сайта: 🖥️ https://ioinitid.github.io/aurora/index.html
 
+Услуги: 🖥️ https://ioinitid.github.io/move/index.html
+
+О нас: 🖥️ https://ioinitid.github.io/about/index.html
+
+Контакты: 🖥️ https://ioinitid.github.io/contacts/index.html
+
 ------------
 
 Сделано с ❤️ в [WEB-ELITIT.RU](https://www.web-elitit.ru "Web-elitit.ru")
